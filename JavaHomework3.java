10c90dfc14c90b7f5676c439f215c964744badc5
tm10067 <110200944+tm10067@users.noreply.github.com>
2023-01-02 21:33:11 +0300

    Homework 3

--- JavaHomework3.java
+++ JavaHomework3.java
@@ -6,17 +6,29 @@ import java.util.ArrayList;
 
 public class JavaHomework3 {
     public static void main(String args[]) {
-        // Задание 1
+        
+        // Задание 1. Пусть дан произвольный список целых чисел, удалить из него четные числа (в языке уже есть что-то готовое для этого)
+        
+        System.out.println("Задание 1");
         List<Integer> list = generateRandomList(10); // генерация списка методом
         System.out.println(list);
         list.removeIf(x -> (x % 2 == 0)); // удаление четных чисел из списка
         System.out.println(list);
-        // Задание 2
-        ArrayList<Integer> array = generateRandomList(10); // генерация списка методом
         
+        // Задание 2. Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее арифметическое из этого списка.
+        
+        System.out.println("Задание 2");
+        ArrayList<Integer> array = generateRandomList(10); // генерация списка методом
+        System.out.println(array);
+        int min = array.stream().mapToInt(x -> x).min().orElse(0);           // используем Stream API
+        int max = array.stream().mapToInt(x -> x).max().orElse(0);           // для нахождения минимального, 
+        double avg = array.stream().mapToDouble(x -> x).average().orElse(0); // максимального и среднего значений списка
+        System.out.printf("Минимальное значение списка: " + min + "\n");
+        System.out.printf("Максимальное значение списка: " + max + "\n");
+        System.out.printf("Среднее арифметическое значение списка: " + avg + "\n");
     }
     
-// Задание 1. Пусть дан произвольный список целых чисел, удалить из него четные числа (в языке уже есть что-то готовое для этого)   
+//  Метод генерации списка для обоих заданий
 
     private static ArrayList<Integer> generateRandomList(int length){
         ArrayList<Integer> list = new ArrayList<>();
@@ -26,8 +38,4 @@ public class JavaHomework3 {
         }
         return list;
     }
-    
-// Задание 2. Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее арифметическое из этого списка.
-    
-    
 }
